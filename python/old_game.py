8b7967abcb0dd844327ec2f425e1df7c2fb977f3
felipejunqueira <felipejleite18@gmail.com>
2026-05-31 15:34:09 -0300

    feat: add old game python matrix script to python folder

---
+++ python/old_game.py
@@ -0,0 +1,4 @@
+matrix = [['n' for _ in range(3)] for _ in range(3)] # [[' ', ' ', ' '], [' ', ' ', ' '], [' ', ' ', ' ']]
+for i in range(len(matrix)):
+    for j in range(len(matrix[i])):
+        print(matrix[i][j])
